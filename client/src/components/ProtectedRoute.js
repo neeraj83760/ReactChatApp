2693891db23296eb771e504ade52e7570fe1aed0
Neeraj <neerajkumarghildiyal@gmail.com>
2023-01-21 04:45:30 +0530

    till now we have completed section 08 lecture 15

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -2,8 +2,9 @@ import React, {useEffect} from 'react'
 import {useNavigate} from 'react-router-dom'
 import {GetCurrentUser} from '../apicalls/users'
 import toast from "react-hot-toast"
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { HideLoader, ShowLoader } from '../redux/loaderSlice'
+import { SetUser } from '../redux/userSlice'
 
 // Just commit check kar raha hu
 
@@ -11,7 +12,7 @@ function ProtectedRoute({children}){
  
   // const [user, setUser] = React.useState(null);
 
-  const {user} = userSelect
+  const {user} = useSelector(state => state.userReducer);
 
   const dispatch = useDispatch(); 
   const navigate = useNavigate();
@@ -23,7 +24,7 @@ function ProtectedRoute({children}){
    const response = await GetCurrentUser();
    dispatch(HideLoader());
    if(response.success){
-    setUser(response.data)
+    dispatch(SetUser(response.data))
     // return true;
 
    }else{
@@ -52,10 +53,27 @@ function ProtectedRoute({children}){
   },[]); 
 
   return (
-    <div>
-    <h1>{user?.name}</h1>
-    <h1>{user?.email}</h1>  
+    <div className='h-screen w-screen bg-gray-100 p-2'>
+    {/* <h1>{user?.name}</h1>
+    <h1>{user?.email}</h1>   */}
+
+    {/* Header */}
+    <div className='flex justify-between p-5'>
+    <div className='flex items-center gap-1'>
+      <i className ="ri-message-3-line text-2xl"></i>
+      <h1 className='text-primary text-2xl uppercase font-bold'>Chatty</h1>
+    </div>
+    <div className='flex gap-1 text-md'>
+      <i class="ri-shield-user-line"></i>
+      <h1 className='underline'>{user?.name}</h1>
+    </div>
+    </div>
+
+    {/* Content - it's nothing but multiple web pages */}
+    <div className='p-5'>
     {children}
+    </div>
+    
     </div>
   )
 }
